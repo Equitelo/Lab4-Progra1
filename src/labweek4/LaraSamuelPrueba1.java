236ec1425d33092fcdef8fca931c7acb536aaa84
Equitelo <samuel_lara@unitec.edu>
2024-08-09 08:26:57 -0600

    Tengo el primero ya hecho, el cual es ingresar una palabra y el programa lo mostrara al reves. Se realizo con un ciclo while el cual tiene un contador que ve la longitud de dicha palabra, se compara si es mayor a cero y cuando ingrese el contador restara.

--- src/labweek4/LaraSamuelPrueba1.java
+++ src/labweek4/LaraSamuelPrueba1.java
@@ -9,18 +9,21 @@ import java.util.Random;
 
 public class LaraSamuelPrueba1 {
     public static void main(String[] args) {
-        Scanner lea=new Scanner(System.in);
+        Scanner lea=new Scanner(System.in).useDelimiter("\n");
         Random aleatorio=new Random();
         
         int menu=0;
         while(menu!=5){
             do{
-                System.out.println("1 - Palabra Alreves");
+                System.out.println("");
+                System.out.println("\n1 - Palabra Alreves");
                 System.out.println("2 - Numero Perfecto");
                 System.out.println("3 - Primos");
                 System.out.println("4 - Votaciones");
                 System.out.println("5 - Salir del sistema");
-
+                System.out.print("Ingresar opcion a realizar: ");
+                menu=lea.nextInt();
+                
                 if(menu<0||menu>5){
                     System.out.println("Opciones no validas! ");
                 }
@@ -28,13 +31,22 @@ public class LaraSamuelPrueba1 {
             }while(menu<0||menu>5);
             
             if(menu==1){
-            
+                System.out.print("Ingresar palabra para revertirla: ");
+                String palabra=lea.next();
+                
+                int reversa=palabra.length()-1;
+                
+                while(reversa>=0){
+                    
+                    System.out.print(palabra.charAt(reversa));
+                    reversa--;
+                }
             }else if(menu==2){
-            
+                
             }else if(menu==3){
-            
+                
             }else if(menu==4){
-            
+                
             }else{
                 System.out.println("Termino.");
             }
